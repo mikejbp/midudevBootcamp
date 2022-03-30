b1cc26433da0f5ed980df624c589baf4445d8d50
mikejbp <74782928+mikejbp@users.noreply.github.com>
2022-03-30 11:32:46 -0400

    First Commits

---
+++ Dev/web-ejemplo/script.js
@@ -0,0 +1,27 @@
+/*const list = [];
+const anotherList = list.concat(157);
+
+const persona = {
+  name: 'Miguel',
+  instagram: '@mikejbp_',
+  age: 24,
+  isTrainee: true,
+  links: ['https://midu.tube','https://linkedin.com/in/mbriceno16']
+};
+
+const sumar = (a, b) => {
+  console.log(a);
+  console.log(b);
+  return a + b;
+};
+
+function restar (a,b) {
+  return a - b;
+};
+
+const op1 = 8;
+const op2 = 2;
+
+console.log(sumar(op1, op2));*/
+
+//
